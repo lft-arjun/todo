7221a62e103f493c296e5d17d4a7d9ab0f38ffed
linux <arjunkoid@gmail.com>
2016-07-12 21:57:48 +0545

    added config

---
+++ configs/database.js
@@ -0,0 +1,11 @@
+var config = {
+        user: 'root',
+        password: 'password',
+        server: 'localhost', 
+        database: 'blog' 
+    };
+
+
+// database.connect();
+
+module.exports = config;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../configs/database');
+var database = mysql.createConnection(config);
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/detail/:id', function(req, res, next) {
+	console.log(database.connect());
+  res.send(req.params.id);
+});
 module.exports = router;
